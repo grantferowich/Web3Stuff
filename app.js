ae3caf507fa77cf46b8c40037845b5af63355057
clariti23 <g.ferowich@gmail.com>
2020-10-31 20:27:20 -0500

    create address

---
+++ app.js
@@ -0,0 +1,13 @@
+var Tx = require('ethereumjs-tx');
+const Web3 = require('web3');
+const url = "https://ropsten.infura.io/v3/288bc65f9d5f47ae8486cf912ebe19dc";
+const web3 = new Web3(url);
+
+// export PRIVATE_KEY_1='0xa8257a994d4d2ec002aa87018edad2578b4aa4cd86eac43f38347408da57dd5d';
+// export PRIVATE_KEY_2='0x11d0ce0e15a1471869170c82d18ab48acd941d0c7b53694d647410745168c3f1'
+
+// let testAccount2 = web3.eth.accounts.create();
+// console.log(testAccount2);
+
+const testAccount1 = '0xd2D43Cf78E99D31Ee1d63fD785751CB97BF33691';
+// const testAccount2 = '0xDF9341F8822a25e9a236e36e4e23B52fc7a6e100';
